24b68996f892823ab9890aceee2cb9ae9cc462b9
Luis <vigo7032@gmail.com>
2026-04-09 15:38:38 -0500

    Suiendo ejercicio01 en c#

---
+++ Ejercicio01/ejercicio01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class ejercicio01
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+            Console.Write("Ingrese su nombre:");
+            nombre= Console.ReadLine();
+            Console.Write("Ingrese su carrera:");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine("\n///////////Impreso con +///////////");
+            Console.WriteLine(nombre+", Bienvenidos al curso de fundamentos de algoritmos de la carrera"+carrera);
+
+            Console.WriteLine("\n///////////Impreso con $///////////");
+            Console.WriteLine($"{nombre},Bienvenidos al curso de fundamentos de algoritmos de la carrera {carrera}"); 
+            Console.ReadKey();
+
+        }
+    }
+}
