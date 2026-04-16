90b3fb28982d5170d1d191290d433f61386e3ba1
Luis <vigo7032@gmail.com>
2026-04-16 15:56:44 -0500

    Actualizando ejercicio3

---
+++ ejercicio02/ejer3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingese numero 1: "))
+num2 = int(input("Ingese numero 2: "))
+
+print("Suma: ",(num1+num2))
+print("Resta: ",(num1-num2))
+print("Multiplicacion: ",(num1*num2))
+print("División: ",(num1/num2))
